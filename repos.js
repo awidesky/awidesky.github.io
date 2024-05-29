9faddad2b102ff57da75e8013737afc5931266a7
Eugene Hong <awidesky@naver.com>
2024-05-29 15:16:50 +0900

    fix indentation and add comments

--- repos.js
+++ repos.js
@@ -1,28 +1,31 @@
 
-function findGithubFile(repo, branch, file, callback)
-{
-    //404 errors in browser are not hideable.
-    //see : https://stackoverflow.com/questions/44019776/fetch-api-chrome-and-404-errors
+function findGithubFile(repo, branch, file, callback) {
+    /*
+     Fetch file's content if exist.
+     Even though it does not exist, that's not a problem.
+     But we cannot suppress 404 error logs in browser.
+     see : https://stackoverflow.com/questions/44019776/fetch-api-chrome-and-404-errors
+    */
     fetch("https://raw.githubusercontent.com/awidesky/" + repo + "/" + branch + "/" + file)
-    .then(function(response) {
-        if(response.ok) {
-            //alert(response.header.get("content-type"));
-            response.text().then((t) => {
-                callback(t);
-            });
-        }
-    });
+        .then(function (response) {
+            if (response.ok) {
+                //alert(response.header.get("content-type"));
+                response.text().then((t) => {
+                    callback(t);
+                });
+            }
+        });
 }
 
-function downloadObjectAsJson(exportObj, exportName){
+function downloadObjectAsJson(exportObj, exportName) {
     //for debug
-  var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
-  var downloadAnchorNode = document.createElement('a');
-  downloadAnchorNode.setAttribute("href", dataStr);
-  downloadAnchorNode.setAttribute("download", exportName + ".json");
-  document.body.appendChild(downloadAnchorNode); // required for firefox
-  downloadAnchorNode.click();
-  downloadAnchorNode.remove();
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href", dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
 }
 
 function getDateDiff(updatedAt, pushedAt) {
@@ -34,15 +37,15 @@ function getDateDiff(updatedAt, pushedAt) {
     const hours = diff.getUTCHours();
     const mins = diff.getUTCMinutes();
     let ret = "updated ";
-    if(year > 0) {
+    if (year > 0) {
         ret += year + "years ago";
-    } else if(month > 0) {
+    } else if (month > 0) {
         ret += month + "months ago";
-    } else if(date > 0) {
+    } else if (date > 0) {
         ret += date + "days ago";
-    } else if(hours > 0) {
+    } else if (hours > 0) {
         ret += hours + "hours ago";
-    } else if(mins > 0) {
+    } else if (mins > 0) {
         ret += mins + "minutes ago";
     } else {
         ret += diff.getUTCSeconds() + "seconds ago";
@@ -54,7 +57,7 @@ function getDateDiff(updatedAt, pushedAt) {
 function getRepoDiv(repo) {
     const div = document.createElement("div");
     div.classList.add("repo");
-    
+
     const title = document.createElement("div");
     const repoLink = document.createElement("a");
     repoLink.href = repo['html_url'];
@@ -62,9 +65,9 @@ function getRepoDiv(repo) {
     repoLink.classList.add("tag");
     repoLink.classList.add("repoLink");
     title.appendChild(repoLink);
-    
+
     const li = repo['license'];
-    if(li != null && li.name != "Other") {
+    if (li != null && li.name != "Other") {
         const license = document.createElement("a");
         license.href = 'javascript:void(0);';
         license.onclick = () => {
@@ -79,63 +82,65 @@ function getRepoDiv(repo) {
         license.classList.add("license");
         title.appendChild(license);
     }
-    
-    if(repo['language'] != null) {
+
+    if (repo['language'] != null) {
         const lang = document.createElement("p");
         lang.textContent = repo['language'];
         lang.classList.add("tag");
         lang.classList.add("lang");
         title.appendChild(lang);
     }
-    
+
     title.classList.add("title");
     div.appendChild(title);
-    
-    
-    if(repo['description'] != "") {
+
+
+    if (repo['description'] != "") {
         const desc = document.createElement("p");
         desc.textContent = repo['description']
         desc.classList.add("desc");
         div.appendChild(desc);
     }
-    
+
     const releaseLink = document.createElement("a");
     releaseLink.href = repo['html_url'] + "/releases";
     releaseLink.textContent = "release";
     div.appendChild(releaseLink);
-    
+
     const updatedAt = document.createElement("p");
     updatedAt.textContent = getDateDiff(repo['updated_at'], repo['pushed_at']);
     updatedAt.classList.add("tag");
     updatedAt.classList.add("updatedAt");
     div.appendChild(updatedAt);
-    
+
     //release 버튼
+    //json crwaling 만든 시점을 기준으로 그 이후에 업데이트되지 않은 레포는 다 넘기고.. 나머지만 긁어오기
     /*
      { //myrepos.json
      "release": false,
      "mavenLib": true, //pom 찾을필요 없이
+     "dev_branch": "dev", // master 말고, 개발 브랜치
      }
      */
-    
+
     return div;
 }
 
 function readProjectJson(repo, div) {
-    
+
     function addMavenDiv(pom) {
         //check if it's deployed to maven central or not
-        if(!pom.includes("<artifactId>nexus-staging-maven-plugin</artifactId>")) return;
-        
+        if (!pom.includes("<artifactId>nexus-staging-maven-plugin</artifactId>")) return;
+
         //denote that it's a maven library project
         const mavenCentral = "https://central.sonatype.com/artifact/io.github.awidesky/" + repo['name'];
-        if(true) {
+        if (true) {
             const btn = document.createElement("button")
-            btn.onclick = function() { window.open(mavenCentral) };
+            btn.onclick = function () { window.open(mavenCentral) };
             btn.textContent = "see in maven central";
             div.appendChild(btn);
         };
     }
-    if(!repo.fork) findGithubFile(repo['name'], repo['default_branch'], "pom.xml", addMavenDiv);
-    
+    if (!repo.fork) findGithubFile(repo['name'], repo['default_branch'], "pom.xml", addMavenDiv);
+
 }
