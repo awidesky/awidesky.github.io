90c1fd1dfbd65fb695bf84423c9aa5187f0570fb
Eugene Hong <awidesky@naver.com>
2024-06-07 21:53:44 +0900

    working TODO cache with debugs

--- TODO.js
+++ TODO.js
@@ -1,28 +1,22 @@
 
-let TODOUpdateTime = new Date((localStorage.hasOwnProperty("TODOUpdateTime")) ? "2000-01-01T01:00:00Z" : localStorage.getItem("TODOUpdateTime"));
+let TODOUpdateTime = new Date((localStorage.hasOwnProperty("TODOUpdateTime")) ? localStorage.getItem("TODOUpdateTime") : "2000-01-01T01:00:00Z");
 let TODOList = [];
 function TODO(repos) {
-    if(localStorage.hasOwnProperty("TODOList")) downloadObjectAsJson(JSON.parse(LZString.decompress(localStorage.getItem("TODOList"))), "TODOList_Cached");
+    if(localStorage.hasOwnProperty("TODOList")) {
+        TODOList = JSON.parse(LZString.decompress(localStorage.getItem("TODOList")));
+        downloadObjectAsJson(TODOList, "TODOList_Cached")
+    }
     const TODORegex = /TODO\s*:/;
     const now = new Date();
-    localStorage.setItem("TODOUpdateTime", now.toString());
     const parentDiv = document.getElementById("TODOs");
     $.when.apply($, repos.map((repo) => {
         const pushedAt = new Date(repo.pushed_at);
-        const monthDiff = now.getMonth() - pushedAt.getMonth() + (12 * (now.getFullYear() - pushedAt.getFullYear()));
-        //if (pushedAt <= TODOUpdateTime || monthDiff > 12) return;
+        if (pushedAt <= TODOUpdateTime) return null;
+        console.log("fetching " + repo.name + " since pushedAt is " + pushedAt.toString() + " and TODOUpdateTime is " + TODOUpdateTime);
         //완성되면 getGithubAPI 쓰기
         return $.getJSON("https://api.github.com/repos/awidesky/" + repo['name'] + "/git/trees/" + repo['latest_branch'] + "?recursive=1").then((files) => {
             files = files.tree.filter((f) => f.type == "blob").filter(testSourceFile); //only check "blob"(file), not "tree"(directory).
             if(files.length == 0) return;
-            const div = document.createElement("div");
-            div.classList.add("TODOdiv");
-            const title = document.createElement("a");
-            title.href = repo['html_url'];
-            title.innerHTML = "<h2>" + repo['name'] + "</h2>";
-            div.appendChild(title);
-            const ul = document.createElement("ul");
-            div.appendChild(ul);
             const promiseList = [];
             files.forEach((f) => promiseList.push(findGithubFile(repo['name'], repo['latest_branch'], f.path).then((raw) => {  //잘 되면 이거 그냥 안으로 넣어버리기?
                 let list = [];
@@ -33,37 +27,37 @@ function TODO(repos) {
                 const lines = raw.split('\n');
                 lines.forEach((s) => {
                     i++;
-                    if (TODORegex.test(s)) {
-                        let surrounding = [];
-                        let ct = 0;
-                        for(let lineCnt = 1; -1 < (i - 1 - lineCnt) && (i - 1 - lineCnt) < lines.length; lineCnt++) {
-                            const str = lines[i - 1 - lineCnt];
-                            if(/\S/.test(str)) {
-                                surrounding.unshift(str.replaceAll("<", "&lt").replaceAll(">", "&gt").replace(/\t/g, "    "));
-                                ct++;
-                            }
-                            if(ct > 3) break;
+                    if (!TODORegex.test(s)) return;
+
+                    let surrounding = [];
+                    let ct = 0;
+                    for (let lineCnt = 1; -1 < (i - 1 - lineCnt) && (i - 1 - lineCnt) < lines.length; lineCnt++) {
+                        const str = lines[i - 1 - lineCnt];
+                        if (/\S/.test(str)) {
+                            surrounding.unshift(str.replaceAll("<", "&lt").replaceAll(">", "&gt").replace(/\t/g, "    "));
+                            ct++;
                         }
-                        const indexOfS = surrounding.length;
-                        ct = 0;
-                        for(let lineCnt = 0; -1 < (i - 1 + lineCnt) && (i - 1 + lineCnt) < lines.length; lineCnt++) {
-                            const str = lines[i - 1 + lineCnt];
-                            if(/\S/.test(str)) {
-                                surrounding.push(str.replaceAll("<", "&lt").replaceAll(">", "&gt").replace(/\t/g, "    "))
-                                ct++;
-                            }
-                            if(ct > 4) break;
-                        }
-                        const indentCorrectedList = trimLeadingWS(surrounding.join("\r\n")).split("\r\n");
-                        indentCorrectedList[indexOfS] = "<span style='color:yellow;'>" + indentCorrectedList[indexOfS] + "</span>";
-                        const surroundingStr = indentCorrectedList.join("<br>").replace(/[\r\n]/g, "");
-                        s = s.substr(s.search(TODORegex));
-                        obj.l = i;
-                        obj.s = s;
-                        obj.c = surroundingStr;
-                        obj.li = "https://github.com/awidesky/" + repo['name'] + "/blob/" + repo['latest_branch'] + "/" + f.path + "#L" + obj.l;
-                        list.push(obj);
+                        if (ct > 3) break;
                     }
+                    const indexOfS = surrounding.length;
+                    ct = 0;
+                    for (let lineCnt = 0; -1 < (i - 1 + lineCnt) && (i - 1 + lineCnt) < lines.length; lineCnt++) {
+                        const str = lines[i - 1 + lineCnt];
+                        if (/\S/.test(str)) {
+                            surrounding.push(str.replaceAll("<", "&lt").replaceAll(">", "&gt").replace(/\t/g, "    "))
+                            ct++;
+                        }
+                        if (ct > 4) break;
+                    }
+                    const indentCorrectedList = trimLeadingWS(surrounding.join("\r\n")).split("\r\n");
+                    indentCorrectedList[indexOfS] = "<span style='color:yellow;'>" + indentCorrectedList[indexOfS] + "</span>";
+                    const surroundingStr = indentCorrectedList.join("<br>").replace(/[\r\n]/g, "");
+                    s = s.substr(s.search(TODORegex));
+                    obj.l = i;
+                    obj.s = s;
+                    obj.c = surroundingStr;
+                    obj.li = "https://github.com/awidesky/" + repo['name'] + "/blob/" + repo['latest_branch'] + "/" + f.path + "#L" + obj.l;
+                    list.push(obj);
                 });
 
                 let exist = {};
@@ -73,42 +67,50 @@ function TODO(repos) {
             //console.log("promiseList \"" + promiseList + "\", type : " + Object.prototype.toString.call(promiseList[0]));
             return $.when.apply($, promiseList).then((...list) => {
                 list = [].concat(...list.filter(Array.isArray))
-                //console.log("asdf \"" + JSON.stringify(list, null, 4) + "\", type : " + Object.prototype.toString.call(list));
                 if (list.length == 0) return null;
 
-                if (ul.childElementCount == 0) parentDiv.appendChild(div);
-                list.forEach((l) => {
-                    const li = document.createElement("li");
-                    const a = document.createElement("a");
-                    a.href = l.li;
-                    a.textContent = "line " + l.l + " of " + l.f;
-                    li.appendChild(a);
-                    const p = document.createElement("p");
-                    p.textContent = l.s;
-                    const tooltip = document.createElement("pre");
-                    tooltip.classList.add("tooltiptext");
-                    tooltip.innerHTML = l.c;
-                    li.appendChild(p);
-                    li.appendChild(tooltip);
-                    ul.appendChild(li);
-                });
                 return { 'name': repo['name'], 'list': list };
             });
         });
-    })).then((...TODOList) => {
-        //TODOList = [].concat(...TODOList.filter(Array.isArray))//이게 문제다
-        TODOList = TODOList.filter(t => t != null);
-        console.log("TODOList : " + JSON.stringify(TODOList, null, 4));
-        let t1 = performance.now();
-        const un = JSON.stringify(TODOList);
-        t1 = performance.now() - t1;
-        console.log("unzipped size : " + un.length + ", time : " + t1);
-        t1 = performance.now();
-        const zi = LZString.compress(JSON.stringify(TODOList));
-        t1 = performance.now() - t1;
-        console.log("zipped size : " + zi.length + ", time : " + t1);
-        downloadObjectAsJson(TODOList, "TODOList");
+    })).then((...gatheredTODOList) => {
+        gatheredTODOList.forEach(newt => {
+            if (newt == null) return;
+            const find = TODOList.find(t => t.name == newt.name);
+            if (find === undefined) {
+                TODOList.push(newt);
+            } else {
+                find.list = newt.list;
+            }
+        });
 
+        TODOList.forEach(t => {
+            const div = document.createElement("div");
+            div.classList.add("TODOdiv");
+            const title = document.createElement("a");
+            title.href = "https://github.com/awidesky/" + t.name;
+            title.innerHTML = "<h2>" + t.name + "</h2>";
+            div.appendChild(title);
+            const ul = document.createElement("ul");
+            div.appendChild(ul);
+            parentDiv.appendChild(div);
+
+            t.list.forEach(l => {
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.href = l.li;
+                a.textContent = "line " + l.l + " of " + l.f;
+                li.appendChild(a);
+                const p = document.createElement("p");
+                p.textContent = l.s;
+                const tooltip = document.createElement("pre");
+                tooltip.classList.add("tooltiptext");
+                tooltip.innerHTML = l.c;
+                li.appendChild(p);
+                li.appendChild(tooltip);
+                ul.appendChild(li);
+            });
+            localStorage.setItem("TODOUpdateTime", now.toString());
+        });
         localStorage.setItem("TODOList", LZString.compress(JSON.stringify(TODOList)));
     }, getGithubApiFail(window.location));
 }
@@ -120,7 +122,7 @@ function testSourceFile(f) {
 }
 
 // my modification of https://stackoverflow.com/a/52432741/9287652
-// change : use <br> not \r\n.
+// change : find smallest common indentation, not the first one
 function trimLeadingWS(str) {
     /*
       Get the initial indentation
